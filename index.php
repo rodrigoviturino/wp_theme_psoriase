74e118fbd3eed9b66a11ce3a812f61241a7c80d5
rodrigoviturino <viturino.souza1@gmail.com>
2020-09-26 02:47:52 -0300

    BAnner e Card Post - aplicado imagem do banner e criação dos cards do post

--- index.php
+++ index.php
@@ -1,9 +1,92 @@
 <?php get_header(); ?>
 
+<main class="pageHome">
 
-<section class="introducao" style="background-image:url('/assets/images/banner-introducao.jpg'); background-size:cover; background-repeat:no-repeat; background-position:center center; padding: 300px 0;">
+    <!-- Banner Introdução -->
+        <section class="introducao" style="background-image:url('<?= get_template_directory_uri();?>/assets/images/banner-introducao.jpg'); background-size:cover; background-repeat:no-repeat; background-position:center center; height:480px"></section>
+    <!-- end Banner Introdução -->
 
-</section>
+    <section class="pageHome__cardPost space-section">
+        <div class="container">
+            <div class="row">
 
+                <div class="col-md-4">
+                    <div class="pageHome__cardPost__item">
+                        <div class="thumbnail">
+                            <img src="<?= get_template_directory_uri(); ?>/assets/images/thumbnail-post.jpg" alt="">
+                        </div>
+                        <div class="info-post">
+                            <h2 class="title">
+                                <a href="#">Preparando Para a sua Próxima Consulta</a>
+                            </h2>    
+                            <p class="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum quasi, voluptate a est eum cumque quod explicabo ipsum sit dicta nobis exercitationem eius neque? Placeat voluptas nostrum aut eos eaque?</p>
+                        </div>
+                    </div>
+                </div>
 
+                <div class="col-md-4">
+                    <div class="pageHome__cardPost__item">
+                        <div class="thumbnail">
+                            <img src="<?= get_template_directory_uri(); ?>/assets/images/thumbnail-post.jpg" alt="">
+                        </div>
+                        <div class="info-post">
+                            <h2 class="title">Titulo Post</h2>    
+                            <p class="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum quasi, voluptate a est eum cumque quod explicabo ipsum sit dicta nobis exercitationem eius neque? Placeat voluptas nostrum aut eos eaque?</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4">
+                    <div class="pageHome__cardPost__item">
+                        <div class="thumbnail">
+                            <img src="<?= get_template_directory_uri(); ?>/assets/images/thumbnail-post.jpg" alt="">
+                        </div>
+                        <div class="info-post">
+                            <h2 class="title">Titulo Post</h2>    
+                            <p class="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum quasi, voluptate a est eum cumque quod explicabo ipsum sit dicta nobis exercitationem eius neque? Placeat voluptas nostrum aut eos eaque?</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4">
+                    <div class="pageHome__cardPost__item">
+                        <div class="thumbnail">
+                            <img src="<?= get_template_directory_uri(); ?>/assets/images/thumbnail-post.jpg" alt="">
+                        </div>
+                        <div class="info-post">
+                            <h2 class="title">Titulo Post</h2>    
+                            <p class="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum quasi, voluptate a est eum cumque quod explicabo ipsum sit dicta nobis exercitationem eius neque? Placeat voluptas nostrum aut eos eaque?</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4">
+                    <div class="pageHome__cardPost__item">
+                        <div class="thumbnail">
+                            <img src="<?= get_template_directory_uri(); ?>/assets/images/thumbnail-post.jpg" alt="">
+                        </div>
+                        <div class="info-post">
+                            <h2 class="title">Titulo Post</h2>    
+                            <p class="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum quasi, voluptate a est eum cumque quod explicabo ipsum sit dicta nobis exercitationem eius neque? Placeat voluptas nostrum aut eos eaque?</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4">
+                    <div class="pageHome__cardPost__item">
+                        <div class="thumbnail">
+                            <img src="<?= get_template_directory_uri(); ?>/assets/images/thumbnail-post.jpg" alt="">
+                        </div>
+                        <div class="info-post">
+                            <h2 class="title">Titulo Post</h2>    
+                            <p class="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum quasi, voluptate a est eum cumque quod explicabo ipsum sit dicta nobis exercitationem eius neque? Placeat voluptas nostrum aut eos eaque?</p>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+
+</main>
 <?php get_footer(); ?>
