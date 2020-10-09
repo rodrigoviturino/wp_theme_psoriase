e611dfc8cd8ddb1124510ba8c069999c15c69d1e
rodrigoviturino <viturino.souza1@gmail.com>
2020-10-09 15:08:08 -0300

    configurando menu mobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,9 +5,21 @@ const t = document.querySelector("#navbarResponsive");
 function handleClick(event) {
   listMenu.classList.toggle("active");
   t.classList.toggle("active");
-  
 }
 btnMobile.addEventListener("click", handleClick);
+
+let menu = document.querySelector(".header");
+let headerClassList = menu.classList;
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY >= 100) {
+    if (!headerClassList.contains("scrollHide")) {
+      headerClassList.add("scrollHide");
+    }
+  } else {
+    headerClassList.remove("scrollHide");
+  }
+});
 
 // const homePosts = document.querySelectorAll(
 //   ".home [data-posts='item'] [data-posts='infos'] p"
